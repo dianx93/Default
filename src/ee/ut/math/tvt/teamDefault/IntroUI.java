39b3b5eb00afa0749fee51148189b3f0613105bc
dianx93 <dianx93@gmail.com>
2014-10-14 14:27:26 +0300

    reads fields from prop files

--- src/ee/ut/math/tvt/teamDefault/IntroUI.java
+++ src/ee/ut/math/tvt/teamDefault/IntroUI.java
@@ -1,10 +1,10 @@
 package ee.ut.math.tvt.teamDefault;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,17 +17,45 @@ import javafx.stage.Stage;
 public class IntroUI {
 
 	public static Scene createIntroScene(Stage stage, Group root) throws FileNotFoundException {
-		Scene scene = new Scene(root, 300, 400);
+		Scene scene = new Scene(root, 340, 480);
 		VBox infoBox = new VBox();
 		infoBox.setPadding(new Insets(20));
-		Scanner fromIntroText = new Scanner(new File("lib\\intro.txt")); //need peaks olema properties failis, mitte txt
-		for (int i = 0; i <9 ; i++) {
-			infoBox.getChildren().add(new Label(fromIntroText.nextLine()));
+		
+		Properties prop = new Properties();
+		InputStream input = null;
+	 
+		try {
+			input = new FileInputStream("application.properties");
+			prop.load(input);
+			infoBox.getChildren().add(new Label(prop.getProperty("teamName")));
+			infoBox.getChildren().add(new Label());
+			infoBox.getChildren().add(new Label("Team Leader"));
+			infoBox.getChildren().add(new Label(prop.getProperty("TeamLeader")));
+			infoBox.getChildren().add(new Label(prop.getProperty("TeamLeaderMail")));
+			infoBox.getChildren().add(new Label());
+			infoBox.getChildren().add(new Label("Team"));
+			infoBox.getChildren().add(new Label(prop.getProperty("TeamMember1")));
+			infoBox.getChildren().add(new Label(prop.getProperty("TeamMember2")));
+			
+			input = new FileInputStream("version.properties");
+			prop.load(input);
+			infoBox.getChildren().add(new Label());
+			infoBox.getChildren().add(new Label("Version: "+prop.getProperty("build.number")));
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		fromIntroText.close();
+		
 		infoBox.getChildren().add(new ImageView(new Image(new FileInputStream("lib\\default.gif"))));
 		root.getChildren().add(infoBox);
-		//version number
 		return scene;
 	}
 

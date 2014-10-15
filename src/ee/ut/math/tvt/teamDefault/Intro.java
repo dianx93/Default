de50ef74b432f0ead955dc59fbb090bf1232f6e4
OttMatiisen <ottmatiisen@hotmail.com>
2014-10-15 18:07:14 +0300

    Working logging, removed unnecessary line from build.xml (comment in previous commit), removed log4j.properties as loggers properties are set from within main class

--- src/ee/ut/math/tvt/teamDefault/Intro.java
+++ src/ee/ut/math/tvt/teamDefault/Intro.java
@@ -1,5 +1,15 @@
 package ee.ut.math.tvt.teamDefault;
 
+
+
+import java.io.IOException;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -7,14 +17,20 @@ import javafx.stage.Stage;
 
 public class Intro extends Application {
 	
+	private static final Logger log = Logger.getLogger(Intro.class);
+	
 	public void start(Stage primaryStage) {
 		try {
+			log.setLevel(Level.INFO);
+			log.addAppender(new FileAppender(new PatternLayout("%d %-5p [%c{1}] %m%n"), "Team Default.log"));
+			
 			Group root = new Group();
 			Scene scene = IntroUI.createIntroScene(primaryStage, root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			log.info("Intro window opened");
 		} catch(Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
 	}
 	

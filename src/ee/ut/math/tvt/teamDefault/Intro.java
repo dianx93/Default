4ba2ca7398f77d114bc179d840e9d955d8fb10e4
Ott Matiisen <ottmatiisen@hotmail.com>
2014-10-30 00:39:37 +0200

    Log4j properly initiated, unnecessary jar removed

--- src/ee/ut/math/tvt/teamDefault/Intro.java
+++ src/ee/ut/math/tvt/teamDefault/Intro.java
@@ -1,9 +1,6 @@
 package ee.ut.math.tvt.teamDefault;
 
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 
 import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
 import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
@@ -31,9 +28,6 @@ public class Intro extends Application {
 				ConsoleUI cui = new ConsoleUI(domainController);
 				cui.run();
 			} else {
-				log.setLevel(Level.INFO);
-				log.addAppender(new FileAppender(new PatternLayout(
-						"%d %-5p [%c{1}] %m%n"), "etc\\Team Default.log"));
 				Group root = new Group();
 				Scene scene = IntroUI.createIntroScene(primaryStage, root);
 				primaryStage.setScene(scene);

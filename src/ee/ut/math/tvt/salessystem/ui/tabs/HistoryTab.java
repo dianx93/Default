4c789e4a366a3f5334b8057e695427d4845cbbc7
dianx93 <dianx93@gmail.com>
2014-10-29 18:21:33 +0200

    JTable table = ... line breaks the code

--- src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
+++ src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
@@ -5,6 +5,8 @@ import java.awt.Component;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
  * labelled "History" in the menu).
@@ -12,14 +14,20 @@ import javax.swing.JTable;
 public class HistoryTab {
     
     // TODO - implement!
+	private SalesSystemModel model;
+	
+	public HistoryTab(SalesSystemModel model) {
+	    this.model = model;
+	  }
 
 	//model = HistoryTableModel
     public HistoryTab() {} 
     
     public Component draw() {
         JPanel panel = new JPanel();
-        //JTable table = new JTable(model.get);
+        //JTable table = new JTable(model.getHistoryTableModel());
         //panel.add(table);
+        
         // TODO - Sales history tabel
         return panel;
     }

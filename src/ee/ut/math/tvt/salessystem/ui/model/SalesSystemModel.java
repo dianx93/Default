d264f5597147ce1fd3bb04dcb545b5389f074f76
dianx93 <dianx93@gmail.com>
2014-10-29 17:52:48 +0200

    added HistoryTableModel to SalesSystemModel

--- src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemModel.java
+++ src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemModel.java
@@ -16,6 +16,9 @@ public class SalesSystemModel {
     
     // Current shopping cart model
     private PurchaseInfoTableModel currentPurchaseTableModel;
+    
+    //Current history model
+    private HistoryTableModel historyTableModel;
 
     private final SalesDomainController domainController;
 
@@ -28,6 +31,7 @@ public class SalesSystemModel {
         
         warehouseTableModel = new StockTableModel();
         currentPurchaseTableModel = new PurchaseInfoTableModel();
+        historyTableModel = new HistoryTableModel();
 
         // populate stock model with data from the warehouse
         warehouseTableModel.populateWithData(domainController.loadWarehouseState());
@@ -40,6 +44,10 @@ public class SalesSystemModel {
 
     public PurchaseInfoTableModel getCurrentPurchaseTableModel() {
         return currentPurchaseTableModel;
+    }
+    
+    public HistoryTableModel getHistoryTableModel() {
+    	return historyTableModel;
     }
     
 }

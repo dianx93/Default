75c964c21b59c27d0acf165fa512c062706970bd
dianx93 <dianx93@gmail.com>
2014-11-06 14:05:55 +0200

    added hibernatedataservice

---
+++ src/ee/ut/math/tvt/salessystem/domain/data/HibernateDataService.java
@@ -0,0 +1,26 @@
+package ee.ut.math.tvt.salessystem.domain.data;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.util.HibernateUtil;
+
+@SuppressWarnings("unchecked")
+public class HibernateDataService {
+
+	private Session session = HibernateUtil.currentSession();
+
+	public List<SoldItem> getSoldItems() {
+		List<SoldItem> result = session.createQuery("from SoldItem").list();
+		return result;
+	}
+
+	public List<StockItem> getStockItems() {
+		List<StockItem> result = session.createQuery("from StockItems").list();
+		return result;
+	}
+}

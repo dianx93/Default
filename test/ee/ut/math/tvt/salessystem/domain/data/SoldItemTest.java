4af7c689506f8703b1034c0e59df8abd04d79f8b
dianx93 <dianx93@gmail.com>
2014-11-13 13:39:41 +0200

    test classes and build that's not finished

---
+++ test/ee/ut/math/tvt/salessystem/domain/data/SoldItemTest.java
@@ -0,0 +1,19 @@
+package ee.ut.math.tvt.salessystem.domain.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SoldItemTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

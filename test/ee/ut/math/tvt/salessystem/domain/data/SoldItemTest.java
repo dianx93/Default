5879d8e59d988c9c537cd9f7fbdb525da4e258eb
dianx93 <dianx93@gmail.com>
2014-11-14 20:14:53 +0200

    tests for solditem and stockitem

--- test/ee/ut/math/tvt/salessystem/domain/data/SoldItemTest.java
+++ test/ee/ut/math/tvt/salessystem/domain/data/SoldItemTest.java
@@ -1,23 +1,31 @@
 package ee.ut.math.tvt.salessystem.domain.data;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class SoldItemTest {
+	
+	private StockItem item;
 
 	@Before
 	public void setUp() throws Exception {
+		item = new StockItem(1l, "TestItem", "for testing", 10.90, 2);
 	}
 
-	// TODO
+	// TODO: check if works
 	public void testGetSum() {
+		SoldItem i = new SoldItem(item, 3);
+		Assert.assertEquals(32.70, i.getSum(), 0.0001);
 		
 	}
 	
-	// TODO
+	// TODO: check if works
 	public void testGetSumWithZeroQuantity() {
+		SoldItem i = new SoldItem(item, 0);
+		Assert.assertEquals(0, i.getSum());
 		
 	}
 
--- test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
+++ test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -1,23 +1,40 @@
 package ee.ut.math.tvt.salessystem.domain.data;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StockItemTest {
-
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 	}
 
-	// TODO
+	// TODO: check if correct
 	public void testClone() {
+		StockItem item = new StockItem(1l, "TestItem", "for testing", 10.90, 2);
+		Object newItem = item.clone();
+		Assert.assertEquals(item, newItem);
 		
 	}
 	
-	// TODO
+	// TODO: check if correct
 	public void testGetColumn() {
+		StockItem item = new StockItem(1l, "TestItem", "for testing", 10.90, 2);
+		Assert.assertEquals(item.getColumn(0), 1l);
+		Assert.assertEquals(item.getColumn(1), "TestItem");
+		Assert.assertEquals(item.getColumn(2), 10.90);
+		Assert.assertEquals(item.getColumn(3), 2);
+		exception.expect(RuntimeException.class);
+		item.getColumn(4);
+		
 		
 	}
 
